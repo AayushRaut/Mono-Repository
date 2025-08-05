2a2d49b21749868078ac04907e3c8d9303680803
Ayush Raut <aayushr512@gmail.com>
2025-08-05 17:22:33 +0530

    chore(app): initialize app with ThemeProvider and ColorThemeProvider wrappers

--- Admin Dashboard/src/main.tsx
+++ Admin Dashboard/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ThemeProvider } from "./context/ThemeProvider.tsx";
+import { ColorThemeProvider } from "./context/CustomeThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <ColorThemeProvider >
+        <App />
+      </ColorThemeProvider>
+    </ThemeProvider>
   </StrictMode>
 );
